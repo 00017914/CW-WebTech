1a483b760dba0c6d3a460a62c2b05ff9fcb190fa
unknown <tboysunova@example.com>
2024-12-04 19:03:07 +0500

    uje final

--- 00017914/visit_booking_form/script.js
+++ 00017914/visit_booking_form/script.js
@@ -1,16 +1,29 @@
 const form = document.querySelector("form");
 const log = document.querySelector("#log");
 
-form.addEventListener(
-  "submit",
-  (event) => {
-    const data = new FormData(form);
-    let output = "";
-    for (const entry of data) {
-      output = `${output}${entry[0]}=${entry[1]}\r`;
-    }
-    log.innerText = output;
-    event.preventDefault();
-  },
-  false,
-);
+
+form.addEventListener("submit", (event) => {
+  const data = new FormData(form);
+  let output = "";
+
+  for (const [key, value] of data.entries()) {
+    output += `${key}=${value}\r\n`;
+  }
+
+  log.innerText = output;
+  event.preventDefault();
+});
+
+function openMenu() {
+  const menu = document.getElementById("menu");
+  if (menu) {
+    menu.style.width = "350px";
+  }
+}
+
+function closeMenu() {
+  const menu = document.getElementById("menu");
+  if (menu) {
+    menu.style.width = "0";
+  }
+}
